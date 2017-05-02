1faf468304c4eb4f32c4bc65e785288376cf83c3
svc-student <svc-student@student.directory.jccc>
2017-05-01 20:02:07 -0500

    classwork 5-01

---
+++ Assets/ExampleWindow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ExampleWindow : EditorWindow {
+
+    public string[] strArray = { "one", "two", "three", "four" };
+    public int selectRoyce = 0;
+    public string s1 = "";
+    public float f1, f2, f3, f4;
+
+    [MenuItem("Custom Tools/Example Window")]
+	static void DisplayWindow()
+    {
+        GetWindow<ExampleWindow>();
+    }
+
+    void OnGUI()
+    {
+        float columnSize = position.width/10;
+
+        GUILayout.Label("Example Window");
+        s1 = GUILayout.TextField(s1);
+
+        GUILayout.Space(10);
+
+        GUILayout.Label("Sliders");
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("f1", GUILayout.MaxWidth(columnSize/2));
+        f1 = GUILayout.HorizontalSlider(f1, 0, 10);
+        f1 = EditorGUILayout.FloatField(f1, GUILayout.MaxWidth(columnSize));
+        GUILayout.Label("f3", GUILayout.MaxWidth(columnSize / 2));
+        f3 = GUILayout.HorizontalSlider(f3, 0, 10);
+        f3 = EditorGUILayout.FloatField(f3, GUILayout.MaxWidth(columnSize));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("f2", GUILayout.MaxWidth(columnSize / 2));
+        f2 = GUILayout.HorizontalSlider(f2, 0, 10);
+        f2 = EditorGUILayout.FloatField(f2, GUILayout.MaxWidth(columnSize));
+        GUILayout.Label("f4", GUILayout.MaxWidth(columnSize / 2));
+        f4 = GUILayout.HorizontalSlider(f4, 0, 10);
+        f4 = EditorGUILayout.FloatField(f4, GUILayout.MaxWidth(columnSize));
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(10);
+
+        selectRoyce = GUILayout.SelectionGrid(selectRoyce, strArray, 4);
+    }
+}
---
+++ Assets/SampleWindow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SampleWindow : EditorWindow {
+
+    public string[] strArray = { "one", "two", "three", "four" };
+    public int selectRoyce = 0;
+    public string s1 = "";
+    public float f1, f2, f3, f4;
+    public bool myBool = false;
+
+    public Texture myTexture;
+    public GUISkin mySkin;
+
+    [MenuItem("cw/sw")]
+    static void DisplayWindow()
+    {
+        GetWindow<SampleWindow>(true);
+    }
+
+    void OnEnable()
+    {
+        myTexture = EditorGUIUtility.Load("Blur.jpg") as Texture;
+        mySkin = EditorGUIUtility.Load("ms.guiskin") as GUISkin;
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = mySkin;
+        GUI.DrawTexture(new Rect(0, 0, position.width, position.height), myTexture);
+        GUILayout.Space(10);
+        GUILayout.Label(s1);
+        GUILayout.Space(10);
+        s1 = GUILayout.TextField(s1);
+        GUILayout.Space(10);
+        selectRoyce = EditorGUILayout.IntSlider(selectRoyce, 0, 10);
+        GUILayout.Space(10);
+        f1 = EditorGUILayout.FloatField(f1, mySkin.GetStyle("textfield"));
+        GUILayout.Space(10);
+        if (GUILayout.Button("Button"))
+        {
+            myBool = !myBool;
+        }
+        GUILayout.Space(10);
+        myBool = EditorGUILayout.Toggle(myBool, mySkin.GetStyle("checkbox"));
+    }
+}
